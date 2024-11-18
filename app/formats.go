12e3a76c0af413f6ae78baf93dcc2a460dde77bf
Sveken <sveken@live.com.au>
2024-11-18 20:56:33 +1100

    Add Formats
    This sets if -F is needed and also the long format bullcrap

--- app/formats.go
+++ app/formats.go
@@ -17,3 +17,24 @@ func QualityFinder(path string) string {
 	}
 	return ""
 }
+
+var (
+	Q1080Ph264forced = "299+ba/137+ba/216+ba/298+ba/136+ba/135+ba/134+ba/133+ba/160+ba"
+)
+
+func SetQuality(QualitySelector string) string {
+	switch QualitySelector {
+	case "Q1080Ph264forced":
+		return Q1080Ph264forced
+	default:
+		return ""
+	}
+}
+
+func doWeNeedDashf(QualityValue string) string {
+	if QualityValue != "" {
+		return "-f"
+	}
+	return ""
+
+}
