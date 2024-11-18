33ca334ea5f033a9cc8d9e8482e1cc48e07597f1
Sveken <sveken@live.com.au>
2024-11-18 15:23:50 +1100

    Folder Generation working and it savves there too and stuff

--- app/main.go
+++ app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"log/slog"
@@ -50,4 +51,30 @@ func disablelisting(next http.Handler) http.Handler {
 		}
 		next.ServeHTTP(w, r)
 	})
+}
+
+func urlhelper(path, prefix string) (string, string, error) {
+	pathParts := strings.Split(path, prefix)
+	if len(pathParts[1]) < 3 {
+		return "", "", errors.New("missing or invalid video URL")
+	}
+	// fixing the https:/ thingy with https:// and getting the final video URL
+	VideoURL := strings.Replace(pathParts[1], "https:/", "https://", 1)
+	//Generate a unique ID to track the video based off string at the end of the video URL
+	lastSlashPosition := strings.LastIndex(pathParts[1], "/")
+	VideoID := pathParts[1][lastSlashPosition+1:]
+	logger.Info("Getting Youtube video", "URL", VideoURL, "VideoID", VideoID)
+	return VideoURL, VideoID, nil
+
+}
+
+// Makes the folder the video lives under using the last bit of the string from the video.
+// This way the video could have the the same name as another video but its still treated as different
+func foldergen(VideoID string) error {
+	err := os.MkdirAll(fmt.Sprintf("./Videos/%s", VideoID), 0755) // Permissions: rwxr-xr-x
+	if err != nil {
+		fmt.Println("Error creating directory:", err)
+		return err
+	}
+	return err
 }
--- app/resodownload.go
+++ app/resodownload.go
@@ -1,44 +1,33 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
 	"os"
 	"os/exec"
-	"strings"
 )
-
-func urlhelper(path, prefix string) (string, error) {
-	pathParts := strings.Split(path, prefix)
-	if len(pathParts[1]) < 3 {
-		return "", errors.New("missing or invalid video URL")
-	}
-	// fixing the https:/ thingy with https:// and getting the final video URL
-	VideoURL := strings.Replace(pathParts[1], "https:/", "https://", 1)
-	//Generate a unique ID to track the video based off string at the end of the video URL
-	lastSlashPosition := strings.LastIndex(pathParts[1], "/")
-	VideoID := pathParts[1][lastSlashPosition+1:]
-	logger.Info("Getting Youtube video", "URL", VideoURL, "VideoID", VideoID)
-	return VideoURL, nil
-
-}
 
 //This actually works and gets everything in avc1/h264 ONLY
 //will Start and fallback in the following order
 //1080P60/1080P30/720P60/720P30/480p/360p/240p/144p
 
 func reso1080Ph264forced(w http.ResponseWriter, r *http.Request) {
-	VideoURL, err := urlhelper(r.URL.Path, "reso1080Ph264forced/")
+	VideoURL, VideoID, err := urlhelper(r.URL.Path, "reso1080Ph264forced/")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+	err = foldergen(VideoID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	outputname := fmt.Sprintf("./Videos/%s/%%(title)s.%%(ext)s", VideoID)
 	process := exec.Command(
 		"./yt-dlp",
 		"-f", "299+ba/137+ba/216+ba/298+ba/136+ba/135+ba/134+ba/133+ba/160+ba",
 		"--remux-video", "mp4",
-		"-o", "./Videos/%(title)s.%(ext)s",
+		"-o", outputname,
 		VideoURL,
 	)
 
